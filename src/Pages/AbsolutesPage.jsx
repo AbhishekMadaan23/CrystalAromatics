299d12d84e37aa2f23c69dc34162e47b5ce0fa50
AbhishekMadaan23 <104679188+AbhishekMadaan23@users.noreply.github.com>
2024-02-06 18:58:25 +0530

    Fixed_UI_Bugs

--- src/Pages/AbsolutesPage.jsx
+++ src/Pages/AbsolutesPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import PaginatedTable from "../Components/PaginatedTable";
 import { AbsolutesData } from "../Data/Data";
-import Footer from "../Components/Footer";
 
 const AbsolutesPage = () => {
   return (
     <div>
       <PaginatedTable Heading="Absolutes" data={AbsolutesData} />
-      <Footer />
     </div>
   );
 };
--- src/Pages/AromatherapyOilPage.jsx
+++ src/Pages/AromatherapyOilPage.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import PaginatedTableV2 from "../Components/PaginatedTableV2";
 import { AromatherapyOilData } from "../Data/Data";
-import Footer from "../Components/Footer";
-import PaginatedTable from "../Components/PaginatedTable";
 
 const AromatherapyOilPage = () => {
   const Heading = "Aromatherapy Oils";
   return (
     <div>
       <PaginatedTableV2 Heading={Heading} data={AromatherapyOilData} />
-      <Footer />
     </div>
   );
 };
